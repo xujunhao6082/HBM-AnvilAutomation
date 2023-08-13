50c7c09f54df4afff0c40f1b316270121a5bc592
xujunhao6082 <xujunhao6082@outlook.com>
2023-08-13 21:52:58 +0800

    New GUI-ChangeMode

--- src/main/java/com/xujunhao6082/hbmanvauto/block/AutoAnvil.java
+++ src/main/java/com/xujunhao6082/hbmanvauto/block/AutoAnvil.java
@@ -1,33 +1,73 @@
 package com.xujunhao6082.hbmanvauto.block;
 
+import com.hbm.blocks.machine.NTMAnvil;
+import com.hbm.main.MainRegistry;
+import com.hbm.tileentity.IGUIProvider;
 import com.xujunhao6082.hbmanvauto.Main;
+import com.xujunhao6082.hbmanvauto.gui.*;
 import com.xujunhao6082.hbmanvauto.item.ItemLoader;
+import cpw.mods.fml.common.network.internal.FMLNetworkHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
 
-public class AutoAnvil extends Block {
+public class AutoAnvil extends Block implements IGUIProvider {
     public final Block oldAnvil;
-    public AutoAnvil(Material material,Block oldAnvil){
+
+    public AutoAnvil(Material material, Block oldAnvil) {
         super(material);
         this.oldAnvil = oldAnvil;
         this.setBlockName(getRegisterName().substring(5));
         this.setHardness(5.0F);
         this.setResistance(100.0F);
         this.setStepSound(Block.soundTypeAnvil);
-        this.setBlockTextureName(Main.MODID+":autoanvil");
+        this.setBlockTextureName(Main.MODID + ":autoanvil");
     }
-    public String getRegisterName(){
-        return this.oldAnvil.getUnlocalizedName()+"_auto";
+
+    public String getRegisterName() {
+        return this.oldAnvil.getUnlocalizedName() + "_auto";
     }
+
     @Override
-    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune){
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
         ArrayList<ItemStack> ret = new ArrayList<>();
-        ret.add(new ItemStack(this.oldAnvil,1));
-        ret.add(new ItemStack(ItemLoader.Items.autotool,1));
+        ret.add(new ItemStack(this.oldAnvil, 1));
+        ret.add(new ItemStack(ItemLoader.Items.autotool, 1));
         return ret;
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z,
+                                    EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+        if (world.isRemote) {
+            return true;
+        } else if (!player.isSneaking()) {
+            FMLNetworkHandler.openGui(player, MainRegistry.instance, 0, world, x, y, z);
+            return true;
+        } else if (player.inventory.getCurrentItem() == null) {
+            FMLNetworkHandler.openGui(player, MainRegistry.instance, 1, world, x, y, z);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Container provideContainer(int i, EntityPlayer entityPlayer, World world, int x, int y, int z) {
+        return (i == 0) ? new ContainerAutoAnvilChangeMode(entityPlayer.inventory,
+                ((NTMAnvil)((AutoAnvil)world.getBlock(x, y, z)).oldAnvil).tier)
+                : null;
+    }
+
+    @Override
+    public GuiScreen provideGUI(int i, EntityPlayer entityPlayer, World world, int x, int y, int z) {
+        return (i == 0) ? new GUIAutoAnvilChangeMode(entityPlayer.inventory,
+                ((NTMAnvil)((AutoAnvil)world.getBlock(x, y, z)).oldAnvil).tier)
+                : null;
     }
 }
---
+++ src/main/java/com/xujunhao6082/hbmanvauto/gui/ContainerAutoAnvilChangeMode.java
@@ -0,0 +1,76 @@
+package com.xujunhao6082.hbmanvauto.gui;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.*;
+import net.minecraft.item.ItemStack;
+
+public class ContainerAutoAnvilChangeMode extends Container {
+
+
+    public int tier;
+
+    public ContainerAutoAnvilChangeMode(InventoryPlayer inventory, int tier) {
+        this.tier = tier;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 9; j++) {
+                this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18 + 56));
+            }
+        }
+
+        for(int i = 0; i < 9; i++) {
+            this.addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 142 + 56));
+        }
+
+    }
+
+    @Override
+    public boolean canInteractWith(EntityPlayer player) {
+        return true;
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer player, int par2) {
+        ItemStack var3 = null;
+        Slot var4 = (Slot) this.inventorySlots.get(par2);
+
+        if(var4 != null && var4.getHasStack()) {
+            ItemStack var5 = var4.getStack();
+            var3 = var5.copy();
+
+            if(par2 == 2) {
+
+                if(!this.mergeItemStack(var5, 3, this.inventorySlots.size(), true)) {
+                    return null;
+                }
+                var4.onSlotChange(var5, var3);
+
+            } else if(par2 <= 1) {
+                if(!this.mergeItemStack(var5, 3, this.inventorySlots.size(), true)) {
+                    return null;
+                } else {
+                    var4.onPickupFromSlot(player, var5);
+                }
+            } else {
+
+                if(!this.mergeItemStack(var5, 0, 2, false))
+                    return null;
+            }
+
+            if(var5.stackSize == 0) {
+                var4.putStack(null);
+            } else {
+                var4.onSlotChanged();
+            }
+
+            var4.onPickupFromSlot(player, var5);
+        }
+
+        return var3;
+    }
+
+    @Override
+    public void onContainerClosed(EntityPlayer player) {
+        super.onContainerClosed(player);
+    }
+}
