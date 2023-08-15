bd7ae5b86e84044a5f5232c82b8b98c524f21f4b
xujunhao6082 <xujunhao6082@outlook.com>
2023-08-15 15:32:35 +0800

    DropItems

--- src/main/java/com/xujunhao6082/hbmanvauto/block/AutoAnvil.java
+++ src/main/java/com/xujunhao6082/hbmanvauto/block/AutoAnvil.java
@@ -13,6 +13,7 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
@@ -46,6 +47,22 @@ public class AutoAnvil extends BlockContainer implements IGUIProvider, ITooltipP
         ret.add(new ItemStack(this.oldAnvil, 1));
         ret.add(new ItemStack(ItemLoader.Items.autotool, 1));
         return ret;
+    }
+
+    @Override
+    public void onBlockHarvested(World world, int x, int y, int z, int meta, EntityPlayer player) {
+        super.onBlockHarvested(world,x,y,z,meta,player);
+        TileEntity te = world.getTileEntity(x, y, z);
+        if (te instanceof TileEntityAutoAnvil) {
+            for (ItemStack item : ((TileEntityAutoAnvil) te).slots) {
+                if (item != null) {
+                    world.spawnEntityInWorld(new EntityItem(world, x, y, z, item));
+                }
+            }
+            if (((TileEntityAutoAnvil) te).task != null) {
+                ((TileEntityAutoAnvil) te).task.cancel();
+            }
+        }
     }
 
     @Override
@@ -88,6 +105,6 @@ public class AutoAnvil extends BlockContainer implements IGUIProvider, ITooltipP
     @Override
     @SuppressWarnings("unchecked")
     public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean b) {
-        list.add(EnumChatFormatting.GOLD+I18n.format("tooltip.auto_anvil.text"));
+        list.add(EnumChatFormatting.GOLD + I18n.format("tooltip.auto_anvil.text"));
     }
 }
