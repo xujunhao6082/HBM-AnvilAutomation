fdbf135543381808085bc2cb262ef862c0ad9b19
xujunhao6082 <xujunhao6082@outlook.com>
2023-08-15 16:01:04 +0800

    VersionFormat

--- src/main/java/com/xujunhao6082/hbmanvauto/Main.java
+++ src/main/java/com/xujunhao6082/hbmanvauto/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static CommonProxy proxy;
     public static final String MODID = "hbmanvauto";
     public static final String NAME = "AutoAnvil For HBM-NTM";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "v1.0";
     @Mod.Instance(Main.MODID)
     public static Main instance;
     @EventHandler
