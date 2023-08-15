7490e8ee12f6de4508684fda1e7cdcce109af68b
xujunhao6082 <xujunhao6082@outlook.com>
2023-08-15 11:22:57 +0800

    Fix up the bug that NBT data can't be read

--- src/main/java/com/xujunhao6082/hbmanvauto/block/BlockLoader.java
+++ src/main/java/com/xujunhao6082/hbmanvauto/block/BlockLoader.java
@@ -1,6 +1,5 @@
 package com.xujunhao6082.hbmanvauto.block;
 
-import com.xujunhao6082.hbmanvauto.Main;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
